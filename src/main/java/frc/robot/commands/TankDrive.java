deb5005a91f81f249c4a75941619c9fa4f269338
cocanot123 <56801303+cocanot123@users.noreply.github.com>
2020-01-08 14:46:59 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,78 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class TankDrive extends Command {
+  public TankDrive() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.m_train);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double m_stickX = Robot.m_oi.getXAxis();
+    double m_stickY = Robot.m_oi.getYAxis();
+    double m_stickZ = Robot.m_oi.getZ();
+    
+
+    if(m_stickX ==0 && m_stickY != 0)
+    {
+      
+        Robot.m_train.setLeftMotor(-m_stickY*RobotMap.SPEED_ADJUST);
+        Robot.m_train.setRightMotor(-m_stickY*RobotMap.SPEED_ADJUST);
+        //Move foward
+    }
+
+    if(m_stickX > 0)
+    {
+      Robot.m_train.setRightMotor(m_stickY*(1-m_stickX)); 
+      Robot.m_train.setLeftMotor(m_stickY);
+      // Left turn
+    }
+    else if(m_stickX < 0)
+    {
+      Robot.m_train.setLeftMotor(m_stickY*(1-(-1*m_stickX)));
+      Robot.m_train.setRightMotor(m_stickY);
+      // Right turn
+    }
+    /**/
+    
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.m_train.setLeftMotor(0);
+    Robot.m_train.setRightMotor(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    this.end();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class Pneumatics extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  //Solenoids
+  Solenoid solenoid1 = new Solenoid(0);
+
+  public void extendSolenoid()
+  {
+    solenoid1.set(true);
+  }
+
+  public void retractSolenoid()
+  {
+    solenoid1.set(false);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
