fcc3ccb442bc973a921f0d6ad05df686f0bea1c3
cocanot123 <56801303+cocanot123@users.noreply.github.com>
2020-02-22 14:25:46 -0500

    Update CollectBall.java

--- ShooterV1/src/main/java/frc/robot/commands/CollectBall.java
+++ ShooterV1/src/main/java/frc/robot/commands/CollectBall.java
@@ -9,15 +9,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class CollectBall extends CommandBase {
   /**
    * Creates a new CollectBall.
    */
-
-  public CollectBall() {
+  
+  Joystick m_stick;
+  public CollectBall(Joystick stick) {
+    m_stick = stick;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(RobotContainer.m_intake);
+    addRequirements(RobotContainer.m_train);
   }
 
   // Called when the command is initially scheduled.
@@ -29,11 +33,12 @@ public class CollectBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double xSpeed;
-    double ySpeed; 
-    double rot;
+    double xSpeed = m_stick.getY() + RobotContainer.m_intake.xSpeedAssist();
+    double ySpeed = m_stick.getX(); 
+    double rot = m_stick.getZ() + RobotContainer.m_intake.rotAssist();
     
     m_intake.collectBall(0.4);
+    drive(xSpeed, ySpeed, rot, false);
   }
 
   // Called once the command ends or is interrupted.
