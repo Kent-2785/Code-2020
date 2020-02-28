ce5918c7779432e56020fa2efec04cc4c767b6bc
cocanot123 <56801303+cocanot123@users.noreply.github.com>
2020-02-27 22:33:29 -0500

    Added BrakeMode

--- ShooterV1/src/main/java/frc/robot/subsystems/ClimberMotors.java
+++ ShooterV1/src/main/java/frc/robot/subsystems/ClimberMotors.java
@@ -39,5 +39,10 @@ public class ClimberMotors extends SubsystemBase {
   {
     arm.set(ControlMode.PercentOutput, speed*-1);
   }
+  
+  public void brakeArm()
+  {
+    arm.setNeutralMode(NeutralMode.brake);
+  }
 
 }
